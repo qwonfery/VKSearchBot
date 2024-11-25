8929929d1be1b46cce7df2379e6f71489d8465f2
qwonfery <nikitavish@list.ru>
2024-11-25 13:43:52 +0300

    Описан контролер, получение данных с пользователя

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import logging
 import os
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram.filters.command import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram import F
 
 from config import BOT_TOKEN
 
@@ -14,28 +17,66 @@ bot = Bot(token=BOT_TOKEN)
 # Диспетчер
 dp = Dispatcher()
 
-# Directory to save downloaded files
-DOWNLOAD_DIR = 'downloads'
-os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+# Определяем состояния
+class Form(StatesGroup):
+    query = State()
+    api_key = State()
+    domains = State()
 
 
-@dp.message(content_types=types.ContentType.DOCUMENT)
-async def handle_document(message: types.Message):
-    user = message.from_user.id
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message,  state: FSMContext):
+    await state.set_state(Form.query)
+    await message.reply("Привет! Отправьте мне CSV/TXT файл с запросами или строку с запросами.")
 
+
+@dp.message(Form.query, F.document)
+async def process_file(message: types.Message, state: FSMContext):
     document = message.document
     file_info = await bot.get_file(document.file_id)
-    file_path = file_info.file_path
-    destination = os.path.join(DOWNLOAD_DIR, document.file_name)
 
-    await bot.download_file(file_path, destination)
-    await message.reply(f"Файл {document.file_name} успешно загружен.")
+    file_bytes = await bot.download_file(file_info.file_path)
+    # Преобразуем содержимое файла в строку
+    queries_string = file_bytes.read().decode("utf-8")
+
+    await state.update_data(queries_string=queries_string)
+
+    await state.set_state(Form.api_key)
+    await message.reply("Теперь отправьте ключ API.")
 
 
-# Хэндлер на команду /start
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+@dp.message(Form.query)
+async def process_string(message: types.Message, state: FSMContext):
+    await state.update_data(queries_string=message.text)
+
+    await state.set_state(Form.api_key)
+    await message.reply("Теперь отправьте ключ API.")
+
+
+@dp.message(Form.api_key)
+async def process_api_key(message: types.Message, state: FSMContext):
+    await state.update_data(api_key=message.text)
+
+    await state.set_state(Form.domains)
+    await message.reply("Теперь отправьте домен или домены, по которым мы будем смотреть позиции (разделите домены запятой).")
+
+
+@dp.message(Form.domains)
+async def process_domains(message: types.Message, state: FSMContext):
+    await state.update_data(domains=message.text)
+
+    user_data = await state.get_data()
+    file_path = user_data.get('file_path')
+    queries_string = user_data.get('queries_string')
+    api_key = user_data.get('api_key')
+    domains = user_data.get('domains')
+
+    # Здесь вы можете обработать полученные данные
+    await message.reply(f"Полученные данные:\nЗапросы: {queries_string}\nAPI Key: {api_key}\nДомены: {domains}")
+
+    # Finish conversation
+    await state.set_state(Form.query)
 
 
 # Запуск процесса поллинга новых апдейтов
